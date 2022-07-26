593cb6e05b1197e98febdd3f1a58580c83dcce42
Vladimir Senchuk <103043323+ctyxer@users.noreply.github.com>
2022-07-26 14:56:32 +0300

    Add SASS

--- app.js
+++ app.js
@@ -49,18 +49,18 @@ function stringData() {
 //getters
 
 app.get('/', (req, res) => {
-    session.loyalPass = true;
+    req.session.loyalPass = true;
     connection.query('SELECT * FROM ITEMS', (err, data, firlds) => {
         if (err) throw err;
         res.render('home', {
             items: data,
-            auth: session.auth,
-            username: session.username
+            auth: req.session.auth,
+            username: req.session.username
         })
     })
 })
 
-app.get('/items/:id', (req, res) => {
+app.get('/item/:id', (req, res) => {
     connection.query("SELECT * FROM items WHERE id=?", [req.params.id],
         (err, data, fields) => {
             if (err) throw err;
@@ -71,31 +71,50 @@ app.get('/items/:id', (req, res) => {
                     res.render('item', {
                         item: data[0],
                         comments: data2,
-                        auth: session.auth,
-                        username: session.username
+                        auth: req.session.auth,
+                        username: req.session.username
                     })
                 })
         });
 })
 
+app.get('/item/:id/change', (req, res) => {
+    connection.query("SELECT * FROM items WHERE id=?", [req.params.id],
+        (err, data, fields) => {
+            if (err) throw err;
+            if(data[0].author != req.session.username)
+            {
+                res.redirect('/');
+            }
+            else
+            {
+                res.render("changeItem", {
+                    item: data[0],
+                    auth: req.session.auth,
+                    username: req.session.username
+                })
+            }
+        });
+})
+
 app.get('/add', (req, res) => {
-    if (session.auth == false) {
+    if (req.session.auth != true) {
         res.redirect('/');
     }
     else {
-        res.render('add', { auth: session.auth, username: session.username })
+        res.render('add', { auth: req.session.auth, username: req.session.username })
     }
 
 })
 
 app.get('/login', (req, res) => {
-    if (session.loyalPass == undefined) { session.loyalPass = true; }
-    res.render('login', { auth: session.auth, loyalPass: session.loyalPass, username: session.username })
+    if (req.session.loyalPass == undefined) { req.session.loyalPass = true; }
+    res.render('login', { auth: req.session.auth, loyalPass: req.session.loyalPass, username: req.session.username })
 })
 
 app.get('/register', (req, res) => {
-    if(session.errRegist == undefined){session.errRegist = true;}
-    res.render('register', { auth: session.auth, errRegist: session.errRegist, username: session.username })
+    if(req.session.errRegist == undefined){req.session.errRegist = true;}
+    res.render('register', { auth: req.session.auth, errRegist: req.session.errRegist, username: req.session.username })
 })
 
 //postes
@@ -104,7 +123,7 @@ app.post('/register', (req, res) => {
     let redir = '/'
     if(req.body.username == '' || req.body.password == '')
     {
-        session.errRegist = "Ни одно поле не может быть пустым";
+        req.session.errRegist = "Ни одно поле не может быть пустым";
         redir = '/register';
         res.redirect(redir);
     }
@@ -115,16 +134,16 @@ app.post('/register', (req, res) => {
             [[req.body.username]], (err, data, fields) => {
                 if (err) throw err;
                 if (data[0] != undefined) {
-                    session.errRegist = "Имя уже занято";
+                    req.session.errRegist = "Имя уже занято";
                     redir = '/register';
                 }
                 else {
                     connection.query('INSERT INTO users (username, password) VALUES (?, ?) ',
                         [[req.body.username], md5(String([req.body.password]))], (err, data, field) => {
                             if (err) throw err;
-                            session.auth = true;
-                            session.errRegist = true;
-                            session.username = [req.body.username][0];
+                            req.session.auth = true;
+                            req.session.errRegist = true;
+                            req.session.username = [req.body.username][0];
                         })
                 }
                 res.redirect(redir);
@@ -135,7 +154,7 @@ app.post('/register', (req, res) => {
 })
 
 app.post('/logout', (req, res) => {
-    session.auth = false;
+    req.session.auth = false;
     res.redirect('/');
 })
 
@@ -144,17 +163,17 @@ app.post('/login', (req, res) => {
     connection.query("SELECT * FROM users WHERE username=?",
         [[req.body.username]], (err, data, field) => {
             if (data[0] == undefined) {
-                session.loyalPass = "Аккаунт не существует";
+                req.session.loyalPass = "Аккаунт не существует";
 
             }
             else if (md5(String([req.body.password])) == String(data[0].password)) {
                 redir = '/';
-                session.auth = true;
-                session.username = [req.body.username][0];
-                session.loyalPass = true;
+                req.session.auth = true;
+                req.session.username = [req.body.username][0];
+                req.session.loyalPass = true;
             }
             else {
-                session.loyalPass = "Неверный логин или пароль";
+                req.session.loyalPass = "Неверный логин или пароль";
             }
             res.redirect(redir);
         })
@@ -162,10 +181,10 @@ app.post('/login', (req, res) => {
 })
 
 
-app.post('/store', (req, res) => {
+app.post('/add', (req, res) => {
     connection.query(
-        "INSERT INTO items (title, image, description) VALUES (?, ?, ?)",
-        [[req.body.title], [req.body.image], [req.body.description]], (err, data, fields) => {
+        "INSERT INTO items (title, image, description, author, date_creating) VALUES (?, ?, ?, ?, ?)",
+        [[req.body.title], [req.body.image], [req.body.description], req.session.username, stringData()], (err, data, fields) => {
             if (err) throw err;
         });
     connection.query("SELECT * FROM items WHERE title=?", [[req.body.title]], (err, data, field) => {
@@ -204,11 +223,11 @@ app.post('/addCommentary', (req, res) => {
         let date = new Date();
         connection.query(
             "INSERT INTO comment_" + String([req.body.id]) + " (author, commentary, date) VALUES (?, ?, ?)",
-            [session.username, [req.body.commentary], stringData()], (err, data, fields) => {
+            [req.session.username, [req.body.commentary], stringData()], (err, data, fields) => {
                 if (err) throw err;
             });
     }
-    res.redirect('/items/' + String([req.body.id]));
+    res.redirect('/item/' + String([req.body.id]));
 })
 
 app.post('/deleteCommentary', (req, res) => {
