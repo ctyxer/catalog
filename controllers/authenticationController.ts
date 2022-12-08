e27ce4b567269ff134b8b73cea98564b03152331
ctyxer <ctyxer.mail@yandex.ru>
2022-12-08 19:50:35 +0300

    add catch error

--- controllers/authenticationController.ts
+++ controllers/authenticationController.ts
@@ -1,90 +1,102 @@
 import { Request, Response } from 'express';
 import { users, PrismaClient } from "@prisma/client";
 import * as argon2 from "argon2";
+import { Logger } from "../logs/logger";
 
 const prisma: PrismaClient = new PrismaClient();
+const logger = new Logger();
 
 export class AuthenticationController {
     async login(req: Request, res: Response) {
-        res.render("login",
-            {
-                error: "",
-                auth: req.session.auth,
-                username: req.session.username,
-            });
-    };
-
-    async register (req: Request, res: Response) {
-        res.render("register",
-            {
-                error: "",
-                auth: req.session.auth,
-                username: req.session.username,
-            });
-    };
-
-    async logining (req: Request, res: Response) {
-        const data = await prisma.users.findFirst({
-            where: {
-                username: req.body.username
-            }
-        })
-        if (data != null) {
-            if (await argon2.verify(String(data.password), String(req.body.password))) {
-                req.session.auth = true;
-                req.session.username = [req.body.username][0];
-                res.redirect("/");
-            }
-            else res.render("login", {
-                error: "Password is not correct",
-                auth: req.session.auth,
-                username: req.session.username,
-            });
-        }
-        else res.render("login", {
-            error: "The user does not exist",
-            auth: req.session.auth,
-            username: req.session.username,
+        logger.catcherErr(() => {
+            res.render("login",
+                {
+                    error: "",
+                    auth: req.session.auth,
+                    username: req.session.username,
+                });
         });
     };
 
-    async logout (req: Request, res: Response) {
-        req.session.auth = false;
-        req.session.username = undefined;
-        res.redirect("/");
+    async register(req: Request, res: Response) {
+        logger.catcherErr(() => {
+            res.render("register",
+                {
+                    error: "",
+                    auth: req.session.auth,
+                    username: req.session.username,
+                });
+        });
     };
 
-    async registering (req: Request, res: Response) {
-        if (req.body.username == "" || req.body.password == "") {
-            res.render('register', {
-                error: "The field cannot be empty",
-                auth: req.session.auth,
-                username: req.session.username
-            });
-        } else {
+    async logining(req: Request, res: Response) {
+        logger.catcherErr(async () => {
             const data = await prisma.users.findFirst({
                 where: {
                     username: req.body.username
                 }
             })
             if (data != null) {
-                res.render('register', {
-                    error: "Username already taken",
+                if (await argon2.verify(String(data.password), String(req.body.password))) {
+                    req.session.auth = true;
+                    req.session.username = [req.body.username][0];
+                    res.redirect("/");
+                }
+                else res.render("login", {
+                    error: "Password is not correct",
                     auth: req.session.auth,
                     username: req.session.username,
                 });
-            } else {
-                await prisma.users.create({
-                    data: {
-                        username: req.body.username,
-                        password: await argon2.hash(String(req.body.password)),
-                        role: "user"
-                    }
-                });
-                req.session.auth = true;
-                req.session.username = [req.body.username][0];
-                res.redirect('/');
             }
-        }
+            else res.render("login", {
+                error: "The user does not exist",
+                auth: req.session.auth,
+                username: req.session.username,
+            });
+        });
+    };
+
+    async logout(req: Request, res: Response) {
+        logger.catcherErr(() => {
+            req.session.auth = false;
+            req.session.username = undefined;
+            res.redirect("/");
+        });
+    };
+
+    async registering(req: Request, res: Response) {
+        logger.catcherErr(async () => {
+            if (req.body.username == "" || req.body.password == "") {
+                res.render('register', {
+                    error: "The field cannot be empty",
+                    auth: req.session.auth,
+                    username: req.session.username
+                });
+            } else {
+                const data = await prisma.users.findFirst({
+                    where: {
+                        username: req.body.username
+                    }
+                })
+                if (data != null) {
+                    res.render('register', {
+                        error: "Username already taken",
+                        auth: req.session.auth,
+                        username: req.session.username,
+                    });
+                } else {
+                    await prisma.users.create({
+                        data: {
+                            username: req.body.username,
+                            password: await argon2.hash(String(req.body.password)),
+                            role: "user"
+                        }
+                    });
+                    req.session.auth = true;
+                    req.session.username = [req.body.username][0];
+                    res.redirect('/');
+                }
+            }
+        });
     };
 }
--- controllers/commentariesController.ts
+++ controllers/commentariesController.ts
@@ -1,30 +1,36 @@
 import { Request, Response } from 'express';
 import { comments, PrismaClient } from "@prisma/client";
+import { Logger } from "../logs/logger";
 
 const prisma: PrismaClient = new PrismaClient();
+const logger = new Logger();
 
-export class CommentariesController { 
-    async add (req: Request, res: Response) {
-        if (req.body.commentary != "") {
-            await prisma.comments.create({
-                data: {
-                    author: String(req.session.username),
-                    commentary: String(req.body.commentary),
-                    date_creating: String(new Date()),
-                    item_id: Number(req.body.id)
+export class CommentariesController {
+    async add(req: Request, res: Response) {
+        logger.catcherErr(async () => {
+            if (req.body.commentary != "") {
+                await prisma.comments.create({
+                    data: {
+                        author: String(req.session.username),
+                        commentary: String(req.body.commentary),
+                        date_creating: String(new Date()),
+                        item_id: Number(req.body.id)
+                    }
+                })
+            }
+            res.redirect("/items/" + String([req.body.id]));
+        });
+    };
+
+    async delete(req: Request, res: Response) {
+        logger.catcherErr(async() => {
+            console.log(Number(req.body.id))
+            await prisma.comments.delete({
+                where: {
+                    id: Number(req.body.idComment)
                 }
             })
-        }
-        res.redirect("/items/" + String([req.body.id]));
+            res.redirect("/items/" + String([req.body.id]));
+        });
     };
-
-    async delete (req: Request, res: Response) {
-        console.log(Number(req.body.id))
-        await prisma.comments.delete({
-            where: {
-                id: Number(req.body.idComment)
-            }
-        })
-        res.redirect("/items/" + String([req.body.id]));
-    };
- }
+}
