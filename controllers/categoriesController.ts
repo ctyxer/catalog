b1b19046ecbef35de86b6d13795fbe67da638ace
Vladimir Senchuk <103043323+ctyxer@users.noreply.github.com>
2022-12-09 19:59:37 +0300

    add categories

---
+++ controllers/categoriesController.ts
@@ -0,0 +1,8 @@
+import { Request, categories, Response } from 'express';
+import fs from "fs";
+import { Logger } from "../logs/logger";
+import { stringData } from '../functions';
+
+export class CategoriesController{
+
+}
